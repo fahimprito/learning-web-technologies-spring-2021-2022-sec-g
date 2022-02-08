4c4edcb158ce2132969099c875e5a894887e0bca
fahim ahmmed mojumder <ahmmedahmmed75@gmail.com>
2022-02-08 09:52:19 +0600

    php task

---
+++ MID_LAB_TASK_04_PHP/php_task_1.php
@@ -0,0 +1,14 @@
+<?php
+
+$length = 10;
+$width = 6;
+$area_of_rectengle = $length*$width;
+echo "The area of a Rectangle = " .$area_of_rectengle. "  ";
+$perimeter= 2 * ($length+$width);
+echo "And the perimeter = " .$perimeter;
+
+
+
+
+
+?>
