7454bd894f83d4ed0a6834f7ff124f6f32bf113d
fahim ahmmed mojumder <ahmmedahmmed75@gmail.com>
2022-02-13 11:41:21 +0600

    1

---
+++ MID_LAB_TASK_05_PHP_FORM/task1_name.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Name input </title>
+</head>
+<body>	
+	<form method="post">
+		<fieldset>
+			<legend>NAME</legend>
+            <input type="text" name="username" value="">
+            <br/>
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+         
+		</fieldset>
+	</form>
+</body>
+</html>
