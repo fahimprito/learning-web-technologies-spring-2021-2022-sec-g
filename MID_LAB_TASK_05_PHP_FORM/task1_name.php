557171a005d0a0327187275d7fe5a343539b4454
fahim ahmmed mojumder <ahmmedahmmed75@gmail.com>
2022-02-13 11:56:27 +0600

    1

--- MID_LAB_TASK_05_PHP_FORM/task1_name.php
+++ MID_LAB_TASK_05_PHP_FORM/task1_name.php
@@ -1,18 +1,45 @@
+<?php 
+	
+
+    $uerror = "";
+    $username = "";
+
+    if(isset($_REQUEST['submit']))
+    {
+		
+	if($_REQUEST['username'] == null){
+		$uerror =  "invalid username!";
+	}else{
+		$username = $_REQUEST['username'];
+	}
+
+}
+
+
+
+	
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title> Name input </title>
 </head>
 <body>	
-	<form method="post">
+	<form method="post" action="#">
+
 		<fieldset>
+            
+
 			<legend>NAME</legend>
             <input type="text" name="username" value="">
             <br/>
             <hr>
             <input type="submit" name="submit" value="Submit">
+
          
-		</fieldset>
+		</fieldset> 
 	</form>
 </body>
 </html>
